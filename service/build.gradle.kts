249ddca656b78c80bce54b507e5cde6baa561206
cheese10yun <cheese10yun@gmail.com>
2020-10-18 00:15:52 +0900

    jacoco rule 설정

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -5,8 +5,32 @@ dependencies {
 tasks.register("testReport") {
     val test by tasks
     val jacocoTestReport by tasks
+    val jacocoTestCoverageVerification by tasks
 
-    dependsOn(test, jacocoTestReport)
+    dependsOn(test, jacocoTestReport, jacocoTestCoverageVerification)
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = "0.90".toBigDecimal()
+            }
+
+            limit {
+                counter = "BRANCH"
+                value = "COVEREDRATIO"
+                minimum = "0.90".toBigDecimal()
+            }
+
+            excludes = listOf(
+                "*.test.*"
+            )
+        }
+    }
 }
 
 tasks.jacocoTestReport {
